35dc1f6da1072365e918572cc192d461fb89d90e
Lukas Graf <lukas.graf@math.uni-augsburg.de>
2022-03-17 14:04:00 +0100

    Made \tau and \nu implicit arguments (I think)

--- src/flow.lean
+++ src/flow.lean
@@ -7,7 +7,7 @@ import TechnicalStuff
 
 noncomputable def μ := real.measure_space.volume
 
-structure edgeflow := 
+structure edgeflow (τ : ℝ) (ν : ℝ):= 
   (fp : ℝ →ₘ[μ] ℝ)
   (h_fp_locint : measure_theory.locally_integrable fp μ)
   (h_fp_0_before_0 : ∀ᵐ θ, θ < 0 → fp θ = 0)
@@ -16,28 +16,28 @@ structure edgeflow :=
   (h_fm_0_before_0 : ∀ᵐ θ, θ < 0 → fm θ = 0)
 
 namespace edgeflow
-  variables (f : edgeflow)
+  variables {τ : ℝ} {ν : ℝ} (f : edgeflow τ ν)
 
   noncomputable def Fp : ℝ → ℝ := λ θ, ∫ z in 0..θ, f.fp z ∂μ
   noncomputable def Fm : ℝ → ℝ := λ θ, ∫ z in 0..θ, f.fm z ∂μ
 
-  noncomputable def q (τ : ℝ) : ℝ → ℝ := λ θ, (f.Fp θ) - (f.Fm (θ + τ))
+  noncomputable def q : ℝ → ℝ := λ θ, (f.Fp θ) - (f.Fm (θ + τ))
 
-  noncomputable def c (τ : ℝ) (ν : ℝ) : ℝ → ℝ := λ θ, τ + (f.q τ θ) / ν
+  noncomputable def c : ℝ → ℝ := λ θ, τ + (f.q θ) / ν
 
-  def weak_flowconservation (τ : ℝ) : Prop := ∀ θ ≥ 0, f.Fm (θ + τ) ≤ f.Fp θ
+  def weak_flowconservation : Prop := ∀ θ ≥ 0, f.Fm (θ + τ) ≤ f.Fp θ
 
-  def flow_respects_capacity (τ : ℝ) (ν : ℝ) : Prop := 
+  def flow_respects_capacity : Prop := 
     ∀ᵐ θ : ℝ, f.fm (θ + τ) ≤ ν
 
-  def queue_operates_at_capacity (τ : ℝ) (ν : ℝ) : Prop := 
-    ∀ᵐ θ : ℝ, if f.q τ θ > 0 then f.fm (θ + τ) = ν else f.fm (θ + τ) = min (f.fp θ) ν
+  def queue_operates_at_capacity : Prop := 
+    ∀ᵐ θ : ℝ, if f.q θ > 0 then f.fm (θ + τ) = ν else f.fm (θ + τ) = min (f.fp θ) ν
 
-  def queue_operates_at_capacity_cum (τ : ℝ) (ν : ℝ) : Prop :=
-    ∀ θ : ℝ, f.Fm (θ + f.c τ ν θ) = f.Fp θ
+  def queue_operates_at_capacity_cum : Prop :=
+    ∀ θ : ℝ, f.Fm (θ + f.c θ) = f.Fp θ
 
-  lemma weak_flowconservation_iff_queue_nonnegative (τ : ℝ) :
-    weak_flowconservation f τ ↔ ∀ θ ≥ 0, f.q τ θ ≥ 0 :=
+  lemma weak_flowconservation_iff_queue_nonnegative :
+    weak_flowconservation f ↔ ∀ θ ≥ 0, f.q θ ≥ 0 :=
   begin
     split,
     {
@@ -54,13 +54,13 @@ namespace edgeflow
       intros hqnonneg θ hθpos,
       specialize hqnonneg θ hθpos,
       calc f.Fm (θ + τ) = f.Fp θ - (f.Fp θ - f.Fm (θ + τ)) : by ring
-                ...     = f.Fp θ - (f.q τ θ) : by refl
+                ...     = f.Fp θ - (f.q θ) : by refl
                 ...     ≤  f.Fp θ : by linarith,
     }
   end 
 
-  lemma respects_cap_of_queue_op_at_cap (τ : ℝ) (ν : ℝ): 
-    queue_operates_at_capacity f τ ν → flow_respects_capacity f τ ν :=
+  lemma respects_cap_of_queue_op_at_cap: 
+    queue_operates_at_capacity f → flow_respects_capacity f :=
   begin
     apply ae_of_implies_and_ae,
     intros x hqatcap,
@@ -73,36 +73,34 @@ namespace edgeflow
     exact min_le_right _ _,
   end
 
-  lemma queue_op_at_cap_cum_of_queue_op_at_cap (τ : ℝ) (ν : ℝ):
-    queue_operates_at_capacity f τ ν → queue_operates_at_capacity_cum f τ ν :=
+  lemma queue_op_at_cap_cum_of_queue_op_at_cap:
+    queue_operates_at_capacity f → queue_operates_at_capacity_cum f :=
   begin
     intros h θ,
-    have hq_depletes_slow : ∀ θ', θ ≤ θ' ∧ θ' < θ + (f.q τ θ)/ν → f.q τ θ' > 0 :=
+    have hq_depletes_slow : ∀ θ', θ ≤ θ' ∧ θ' < θ + (f.q θ)/ν → f.q θ' > 0 :=
     begin
       sorry
     end,
-    have h : ∀ᵐ z : ℝ ∂μ, z ∈ set.interval_oc (θ + τ) (θ + (f.q τ θ)/ν + τ) → f.fm z = (λ θ, ν) z 
+    have h : ∀ᵐ z : ℝ ∂μ, z ∈ set.interval_oc (θ + τ) (θ + (f.q θ)/ν + τ) → f.fm z = (λ θ, ν) z 
       := sorry,
     have hνpos : ν ≠ 0 
       := sorry,
-    show ∫ z in 0..θ + f.c τ ν θ, f.fm z ∂μ = f.Fp θ,
+    show ∫ z in 0..θ + f.c θ, f.fm z ∂μ = f.Fp θ,
     rw ← interval_integral.integral_add_adjacent_intervals 
             (interval_integrable_of_locally_integrable f.fm 0 (θ + τ) f.h_fm_locint)
-            (interval_integrable_of_locally_integrable f.fm (θ + τ) (θ + f.c τ ν θ) f.h_fm_locint),
-    show f.Fm (θ + τ) + ∫ z in θ + τ..θ + f.c τ ν θ, f.fm z ∂μ = f.Fp θ,
-    show f.Fm (θ + τ) + ∫ z in θ + τ..θ + (τ + (f.q τ θ) / ν), f.fm z ∂μ = f.Fp θ,
+            (interval_integrable_of_locally_integrable f.fm (θ + τ) (θ + f.c θ) f.h_fm_locint),
+    show f.Fm (θ + τ) + ∫ z in θ + τ..θ + f.c θ, f.fm z ∂μ = f.Fp θ,
+    show f.Fm (θ + τ) + ∫ z in θ + τ..θ + (τ + (f.q θ) / ν), f.fm z ∂μ = f.Fp θ,
     rw add_comm τ _,
     rw ← add_assoc,
     rw interval_integral.integral_congr_ae h,
     simp,
-    show f.Fm (θ + τ) + ∫ (x : ℝ) in θ + τ..θ + f.q τ θ / ν + τ, ν = f.Fp θ,
+    show f.Fm (θ + τ) + ∫ (x : ℝ) in θ + τ..θ + f.q θ / ν + τ, ν = f.Fp θ,
     rw interval_integral.integral_const,
-    calc f.Fm (θ + τ) + (θ + f.q τ θ / ν + τ - (θ + τ)) • ν 
-                    = f.Fm (θ + τ) + (f.q τ θ / ν) • ν : by ring_nf
-                ... = f.Fm (θ + τ) + f.q τ θ : by finish
+    calc f.Fm (θ + τ) + (θ + f.q θ / ν + τ - (θ + τ)) • ν 
+                    = f.Fm (θ + τ) + (f.q θ / ν) • ν : by ring_nf
+                ... = f.Fm (θ + τ) + f.q θ : by finish
                 ... = f.Fm (θ + τ) + (f.Fp θ - f.Fm (θ + τ)) : by refl
                 ... = f.Fp θ : by ring_nf,
   end
-
 end edgeflow
-
